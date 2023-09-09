5d230ba93a6044aed68901b2f81c1abd6c76e90a
Adeshola Ayomide <josephay125d@gmail.com>
2023-09-09 07:00:30 -0700

    up to date

--- src/App.js
+++ src/App.js
@@ -1,21 +1,71 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
 import PageError from './PageError';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import About from './About';
+import Card from './Card';
+
 function App() {
+  // useEffect(() => {
+  //   const sections = document.querySelectorAll('.animate');
+  //   const options = {
+  //     root: null,
+  //     rootMargin: '0px',
+  //     threshold: 0.5,
+  //   };
+  //   const observer = new IntersectionObserver((entries) => {
+  //     entries.forEach((entry) => {
+  //       if (entry.isIntersecting) {
+  //         entry.target.classList.add('active');
+  //       } else {
+  //         entry.target.classList.remove('active');
+  //       }
+  //     });
+  //   }, options);
+
+  //   sections.forEach((Bloglist) => {
+  //     observer.observe(Bloglist);
+  //   });
+
+  //   return () => {
+  //     observer.disconnect();
+  //   };
+  // }, []);
   return (
     <Router>
       <div className="App">
         <Navbar></Navbar>
+          
         <div className="content">
           <Switch>
 
             <Route exact path="/">
               <Home />
+              <h2 className='text-center fw-bold'>EFFECT</h2>
+              <div className='d-md-flex mt-5 mx-auto col-12 text-center justify-content-between'>
+              <Card 
+                   name="Reach your Goals "
+                   image="https://img.freepik.com/free-photo/beautiful-shot-sea-with-windmills-factory-distance-cloudy-sky_181624-11846.jpg?w=740&t=st=1693580433~exp=1693581033~hmac=2789095bf13b3fd77450e6112467a52a739b2c1cff0c3a6b39b1dfda783bdaab"
+                   style=" mx-2 px-md-4  border-none card mt-3  text-dark"
+                   paragraph=" We are committed to helping smart and ambitious young African women live their best lives"
+                  />
+              <Card 
+                   name="Reach your Goals "
+                   image="https://img.freepik.com/free-photo/beautiful-shot-sea-with-windmills-factory-distance-cloudy-sky_181624-11846.jpg?w=740&t=st=1693580433~exp=1693581033~hmac=2789095bf13b3fd77450e6112467a52a739b2c1cff0c3a6b39b1dfda783bdaab"
+                   style=" mx-2  border-none card mt-3  text-dark"
+                   paragraph=" We are committed to helping smart and ambitious young African women live their best lives"
+                  />
+              <Card 
+                   name="Reach your Goals "
+                   image="https://img.freepik.com/free-photo/beautiful-shot-sea-with-windmills-factory-distance-cloudy-sky_181624-11846.jpg?w=740&t=st=1693580433~exp=1693581033~hmac=2789095bf13b3fd77450e6112467a52a739b2c1cff0c3a6b39b1dfda783bdaab"
+                   style=" mx-2  border-none card mt-3  text-dark"
+                   paragraph=" We are committed to helping smart and ambitious young African women live their best lives"
+                  />
+                  </div>
             </Route>
 
             <Route path="/Create">
--- src/Bloglist.js
+++ src/Bloglist.js
@@ -1,24 +1,57 @@
+import React from "react";
 import {Link} from "react-router-dom"
 
 
 const Bloglist = ({ blogs, title, }) => {
     return (
-        <div>
-        <div className="d-flex">
-        <div className="">
-       <h5>Welcome to Jinad's blog</h5><br />
-       <h6> Clean Water and Sanitation to the Forefront of Change</h6>
-       <p>At Jinad's, we are passionate about making a difference in the world by addressing one of the most critical issues of our time: access to clean water and sanitation. Our mission is aligned with the United Nations' Sustainable Development Goal 6, which aims to ensure availability and sustainable management of water and sanitation for all. Through this platform, we aspire to educate, inspire, and drive action towards a future where every individual has access to clean and safe water resources</p>
+        <>
+        {/* <div className="App">
+        <section blogs="animate fade-up"> */}
+        <div id="carouselExampleFade" className="carousel slide carousel-fade">
+    <div className="carousel-inner ">
+      <div className="carousel-item active">
+      <div className="get-position rounded ms-md-5 px-3 px-md-5  mx-auto " >
+       <h3 className="px-3">Welcome to Jinad's blog</h3><br />
+       <h6 className="px-3"> Clean Water and Sanitation to the Forefront of Change</h6>
+       <p className="col-md-6 col-12 carousel-text px-3 text-light">At Jinad's, we are passionate about making a difference in the world by addressing one of the most critical issues of our time: access to clean water and sanitation. Our mission is aligned with the United Nations' Sustainable Development Goal 6, which aims to ensure availability and sustainable management of water and sanitation for all. Through this platform, we aspire to educate, inspire, and drive action towards a future where every individual has access to clean and safe water resources</p>
        </div>
-        <div className="image-holder">
-        <img src="https://i.pinimg.com/564x/c5/bd/56/c5bd5656e0b104927a39fe723643e6aa.jpg" alt=""className="polluted"/>
-        </div>
+        <img  src="https://img.freepik.com/free-photo/macro-green-leaf_23-2148212007.jpg?size=626&ext=jpg&ga=GA1.1.26054885.1689417218&semt=ais" className="d-block px-md-5  px-2 w-100 rounded" alt="..."/>
+      </div>
+      <div className="carousel-item">
+      <div className="get-position rounded ms-md-5 px-3  px-md-5  mx-auto " >
+       <h3>Welcome to Jinad's blog</h3><br />
+       <h6> Clean Water and Sanitation to the Forefront of Change</h6>
+       <p className="col-md-6 col-12 text-light px-3 carousel-text">At Jinad's, we are passionate about making a difference in the world by addressing one of the most critical issues of our time: access to clean water and sanitation. Our mission is aligned with the United Nations' Sustainable Development Goal 6, which aims to ensure availability and sustainable management of water and sanitation for all. Through this platform, we aspire to educate, inspire, and drive action towards a future where every individual has access to clean and safe water resources</p>
+       </div>
+        <img src="https://img.freepik.com/free-photo/beautiful-shot-sea-with-windmills-factory-distance-cloudy-sky_181624-11846.jpg?w=740&t=st=1693580433~exp=1693581033~hmac=2789095bf13b3fd77450e6112467a52a739b2c1cff0c3a6b39b1dfda783bdaab" className="d-block px-md-5 px-3 w-100 rounded" alt="..."/>
+      </div>
+      <div className="carousel-item">
+      <div className="get-position rounded ms-md-5 px-3 px-md-5  mx-auto " >
+       <h3>Welcome to Jinad's blog</h3><br />
+       <h6> Clean Water and Sanitation to the Forefront of Change</h6>
+       <p className="col-md-6 col-12 text-light px-3 carousel-text">At Jinad's, we are passionate about making a difference in the world by addressing one of the most critical issues of our time: access to clean water and sanitation. Our mission is aligned with the United Nations' Sustainable Development Goal 6, which aims to ensure availability and sustainable management of water and sanitation for all. Through this platform, we aspire to educate, inspire, and drive action towards a future where every individual has access to clean and safe water resources</p>
+       </div>
+        <img src="https://img.freepik.com/premium-photo/silhouettes-animals-terrain-with-ecological-problems-polluted-air-by-factories-toxic-waste-water-pollution-generative-ai_131265-169.jpg?size=626&ext=jpg&ga=GA1.1.26054885.1689417218&semt=ais" className="d-block px-md-5 px-3 w-100 rounded" alt="..."/>
+      </div>
+    </div>
+  
+    <button className="carousel-control-prev px-2 fw-bold" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span className="visually-hidden">Previous</span>
+    </button>
+  
+    <button className="carousel-control-next  px-2 fw-bold" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+      <span className="carousel-control-next-icon" aria-hidden="true"></span>
+      <span className="visually-hidden">Next</span>
+    </button>
+  </div>
+ 
+ 
        
-        </div>
-        <div className="blog-list">
+        <div classNameName="blog-list">
             <p>{title}</p>
             {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
+                <div classNameName="blog-preview" key={blog.id}>
                     <Link to= {`/blogs/${blog.id}`}>
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
@@ -26,9 +59,9 @@ const Bloglist = ({ blogs, title, }) => {
                 </div>
             ))
             }
-        </div>
-      
-        </div>
+        </div>        
+        </>
+        
     );
 }
 
